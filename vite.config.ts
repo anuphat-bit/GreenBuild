6bf62d516ae0c0f3d22a97da9ca863687b90e5f7
anuphat-bit <anupha.t@lawasri.tru.ac.th>
2026-02-19 16:17:02 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -21,3 +21,27 @@ export default defineConfig(({ mode }) => {
       }
     };
 });
+import path from 'path';
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, '.', '');
+    return {
+      base: '/GreenBuild/', // *** เพิ่มบรรทัดนี้ ***
+      server: {
+        port: 3000,
+        host: '0.0.0.0',
+      },
+      plugins: [react()],
+      define: {
+        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+      },
+      resolve: {
+        alias: {
+          '@': path.resolve(__dirname, '.'),
+        }
+      }
+    };
+});
